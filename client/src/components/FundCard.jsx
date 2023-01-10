ed3a6c4b0b2f908a2c2f13d6aff4f65bf9117573
eniiyanu <depopf89@gmail.com>
2023-01-11 00:04:07 +0100

    added a comment

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -5,7 +5,7 @@ import { daysLeft } from '../utils';
 
 const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
   const remainingDays = daysLeft(deadline);
-  
+  //fundcard query
   return (
     <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
