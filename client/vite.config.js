30c0ba751c5a715924b37bd01b2dd15d8d9af5c3
eniiyanu <depopf89@gmail.com>
2023-01-10 22:30:16 +0100

    Resolved the vite.config.js error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,8 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [
-      react(),
-    ],
-  });
-};
+import { defineConfig } from 'vite'
+// ...
+export default defineConfig({
+  // ...
+  define: {
+    'process.env': {}
+  }
+})
